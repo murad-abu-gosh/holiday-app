23359956c7d1a4267c2f058e3150042356a11f5d
Murad Abu-Gosh <muradabgosh@gmail.com>
2024-12-14 23:24:27 +0200

    changed API route to '/'

--- app/gemini_api_handler.py
+++ app/gemini_api_handler.py
@@ -3,7 +3,7 @@ import random
 from app.services.gemini_api import generate_10_sentence_objects
 gemini_app = Flask(__name__)
 
-@gemini_app.route('/greetings', methods=['GET'])
+@gemini_app.route('/', methods=['GET'])
 def random_numbers():
     # Get the 'count' parameter from the request, default to 10 if not provided
     # count = request.args.get('count', default=10, type=int)
