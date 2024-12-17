f0f62a0876f75cfb3ad0f324c4a30304bfd4f75a
vov62 <avivovgen1@gmail.com>
2024-12-17 12:05:04 +0200

    add terraform files

---
+++ frontend_container/server-script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo yum update -y
+sudo yum install -y docker
+sudo service docker start
+# sudo usermod -a -G docker ec2-user
+
+# pull docker image from hub
+sudo docker pull vovhubdoc/frontend_container
+sudo docker run -p 5000:5000 frontend_container
+
+echo "running frontend_container..."
+
+
+
+
+
+
+
