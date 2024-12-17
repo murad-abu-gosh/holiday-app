9a35c2b3d14885875b6b903a8f8d6f16f579fb03
vov62 <avivovgen1@gmail.com>
2024-12-17 14:21:46 +0200

    some changes

--- api_container/config.py
+++ api_container/config.py
@@ -9,6 +9,7 @@ class Config:
     MONGO_URI = os.getenv('MONGO_URI', 'mongodb://localhost:27017')
     MONGO_DB_NAME = os.getenv('MONGO_DB_NAME', 'holiday_db')
     HOLIDAY_API_KEY = os.getenv('HOLIDAY_API_KEY', '')
-    HOLIDAY_API_URL = 'https://holidays.abstractapi.com/v1/'
+    HOLIDAY_API_URL = 'http://localhost:5002'
     GEMINI_API_KEY = "AIzaSyCm97Bj5ZXfmKWeh7KpL0wL80uom6afAUE"
-    AWS_REGION = "eu-central-1"
+    AWS_REGION = 'us-west-2'
+    DATABASE_HANDLER_URL = "http://localhost:5001"
